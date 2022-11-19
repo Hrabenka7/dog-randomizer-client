590259feab1e412872b03589a8a62143e4382b5c
Hrabenka7 <markettka@email.cz>
2022-11-19 16:16:01 +0100

    add dropdowns and buttons

--- src/App.js
+++ src/App.js
@@ -6,40 +6,36 @@ import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
  
 
-/* import React, { useEffect, useState } from "react";
-export default function App() {
-  const [data, setData] = useState({});
-  const loadData = async () => {
-    const res = await fetch("https://api.agify.io/?name=michael");
-    setData(await res.json());
-  };
-  useEffect(() => {
-    loadData();
-    return () => {};
-  }, []);
-  return <p>{data.name}</p>;
-}
- */
-
 function App() {
   const [data, setData] = useState({});
+  const [breeds, setBreeds] = useState({});
 
-  const loadData = async () => {
+  const loadRandomImage = async () => {
     const res = await fetch("/random");
     console.log('data fetched')
     setData(await res.json());
   };
-  
+
+  const loadAllBreeds = async () => {
+    const res = await fetch("/allBreeds");
+    console.log('breeds fetched')
+    setBreeds(await res.json());
+  };
+
   useEffect(() => {
-    loadData();
+    loadRandomImage();
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    loadAllBreeds();
     return () => {};
   }, []);
 
  return (
   <React.Fragment>
     <div className="wrapper">
-      <button onClick={loadData}>Next</button>
-      <Card data={data} loadData={loadData}></Card>
+      <Card data={data} loadImage={loadRandomImage} breeds={breeds}></Card>
     </div>
   </React.Fragment>
   ); 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { Image } from 'primereact/image';
+import { Dropdown } from 'primereact/dropdown';
 import "./Card.css";
 
-const Card = (props ) => {
+function Card(props) {
+    console.log('props breed', props.breeds)
+    const [selectedBreed, setSelectedBreed]= useState(null);
+    
+    const onBreedChange = (e) => {
+        setSelectedBreed(e.value);
+    }
+    
     return (
+        <React.Fragment>
         <div>
             <div className="card-holder">
             <div className="card">
@@ -12,12 +21,14 @@ const Card = (props ) => {
             <h3> Breed: {props.data?.breed}</h3>
             <Image src={props.data?.message} alt="Image" width="250" />
             <Button label="Favourite" icon="pi pi-heart" />
-            <Button label="Next" onClick={props.loadData}/>
-            <Button label="Like" onClick={props.loadData}/>
-            <h2> Here add filters</h2>
+            <Button label="Next" onClick={props.loadImage}/>
+            <Button label="Like" onClick={props.loadImage}/>
+           {/*  <Dropdown value={selectedBreed} options={props.breeds.message} onChange={onBreedChange} optionLabel={props.breeds.message} placeholder="Select a Breed"/> */}
             </div>
         </div>
         </div>
-    );
+        </React.Fragment>
+    )
+
 }
 export default Card
