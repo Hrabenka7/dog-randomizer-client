4354e5c3f6b4e5b6665afa5f9d3e59ac0f7b9dfc
Hrabenka7 <markettka@email.cz>
2022-11-19 12:40:20 +0100

    add buttons and loading image logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
+
+/* import React, { useEffect, useState } from "react";
+export default function App() {
+  const [data, setData] = useState({});
+  const loadData = async () => {
+    const res = await fetch("https://api.agify.io/?name=michael");
+    setData(await res.json());
+  };
+  useEffect(() => {
+    loadData();
+    return () => {};
+  }, []);
+  return <p>{data.name}</p>;
+}
+ */
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState({});
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+  const loadData = async () => {
+    const res = await fetch("/random");
+    console.log('data fetched')
+    setData(await res.json());
+  };
+  
+  useEffect(() => {
+    loadData();
+    return () => {};
   }, []);
 
-  return (
-    <div class="wrapper">
-      <Card></Card>
+ return (
+  <React.Fragment>
+    <div className="wrapper">
+      <button onClick={loadData}>Next</button>
+      <Card data={data} loadData={loadData}></Card>
     </div>
-  );
-
-
-
-  
+  </React.Fragment>
+  ); 
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,23 @@
+import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { Image } from 'primereact/image';
 import "./Card.css";
 
-
-function Card() {
-    return(
-        <div class="card-holder">
-            <div class="card">
+const Card = (props ) => {
+    return (
+        <div>
+            <div className="card-holder">
+            <div className="card">
             <h1> Dog Randomizer</h1>
-            <h3> Breed: Spagetti</h3>
-            <Image src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" alt="Image" width="550" />
-            <Button label="Next"/>
-            <Button label="Like"/>
-            <Button label="Like Mark"/>
+            <h3> Breed: {props.data?.breed}</h3>
+            <Image src={props.data?.message} alt="Image" width="250" />
+            <Button label="Favourite" icon="pi pi-heart" />
+            <Button label="Next" onClick={props.loadData}/>
+            <Button label="Like" onClick={props.loadData}/>
             <h2> Here add filters</h2>
             </div>
         </div>
-    )
+        </div>
+    );
 }
-
 export default Card
