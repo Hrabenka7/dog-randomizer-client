7cf5e2009a19be5b2c5a66243e2e3a8f62cbe552
Hrabenka7 <markettka@email.cz>
2022-11-20 19:53:03 +0100

    logic of favourites and dropdowns

--- src/components/Breed.js
+++ src/components/Breed.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Dropdown } from 'primereact/dropdown';
 import "./Breed.css";
-import { capitalize} from '../helpers/string-transform-helper';
 import Photo from './Photo';
 import axios from 'axios';
 
@@ -10,15 +9,42 @@ function Breed() {
 
   const [breeds, setBreeds] = useState({});
   const [breed, setBreed] = useState(null);
+  const [subBreeds, setSubBreeds] = useState({}); 
+  const [subBreed, setSubBreed] = useState(null);
+  const [subBreedStatus, setSubBreedsStatus] = useState(false);
+  const [subBreedsPlaceholder, setSubBreedsPlaceholder] = useState("No Breed Selected");
 
   useEffect(() => {
     axios.get("/allBreeds").then((res) => {
-      console.log('breeds', res)
+      console.log('breeds:', + res)
       setBreeds(res.data)})
     }, []);
 
   const onBreedChange = (e) => {
-    setBreed(e.value);
+    console.log("onBreedChange", e.value)
+	setBreed(e.value);
+	if (e.value === undefined) {
+		setSubBreedsStatus(false);
+		setSubBreedsPlaceholder("No breed selected");
+	}
+	else {
+		if (e.value.subBreeds.length > 0) {
+			setSubBreeds(e.value.subBreeds);
+			setSubBreed(undefined);
+			setSubBreedsPlaceholder("Select a Sub-breed");
+			setSubBreedsStatus(true);
+		}
+		else {
+			// If the breed has no subbreeds, leave the dropdown disable and show corresponding message
+			setSubBreeds(undefined);
+			setSubBreedsPlaceholder("This breed has no sub-breeds");
+			setSubBreedsStatus(false);
+		}
+	}
+  }
+
+  const onSubBreedChange = (e) => {
+	setSubBreed(e.value);
   }
 
     return (
@@ -26,6 +52,8 @@ function Breed() {
         (
         <React.Fragment>
 			<Dropdown value={breed} options={breeds} onChange={onBreedChange} optionLabel="breedName" placeholder="Select a Breed" showClear />	
+			<br></br>
+			<Dropdown value={subBreed} options={subBreeds} onChange={onSubBreedChange} placeholder={subBreedsPlaceholder} disabled={!subBreedStatus} showClear />	
 			<Photo breed={breed}/>
 			{/* <Button label="Favourite" icon="pi pi-heart" /> */}
         </React.Fragment>
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,11 +4,12 @@ import { ProgressSpinner } from 'primereact/progressspinner';
 import "./Photo.css";
 import { Button } from 'primereact/button';
 import { capitalize} from '../helpers/string-transform-helper';
-
+import axios from 'axios';
 
 function Photo(props) {  
 	console.log('Photo Rendered');
 	const [imageData, setImageData] = useState(null);
+
 
 	useEffect(() => {
 		console.log('Use Effect in Photo was called')
@@ -17,23 +18,79 @@ function Photo(props) {
 	}, []);
 
 	useEffect(() => {
-		if(props && props.breed) {
-			console.log('breed',props.breed)
-			loadFilteredImage(props.breed.breedName)
+		if (!props) {
+			return;
 		}
-	  }, [props.breed]);
 
+		if (props.breed) {
+			console.log('breed: ' + props.breed);
+			loadFilteredImage(props.breed.breedName);
+		}
+		else {
+			loadRandomImage();
+		}
+	}, [props.breed]);
 
-	const loadRandomImage = async () => {
-		const res = await fetch("/random");
-		setImageData(await res.json())
+	const loadRandomImage = () => {
+		axios.get("/random").then((res) => {
+			console.log('random', res);
+			setImageData(res.data);
+		})
 	};
 	
 	const loadFilteredImage = async (breed) => {
 		console.log('breed', breed);
 		const res = await fetch(`/breed/param?breed=${breed}`);
 		setImageData(await res.json());
-	  };
+	};
+
+	const saveFavoriteDog = (dogName) => {
+		// Load favorite dogs array from local storage
+		var favoriteDogs = JSON.parse(window.localStorage.getItem("favorite_dogs"));
+
+		// If the array doesn't exist, create it
+		if (!favoriteDogs) {
+			favoriteDogs = [];
+		}
+
+		// Add the new dog to the array
+		favoriteDogs.push(dogName);
+
+		// Save the dog to the local store
+		window.localStorage.setItem("favorite_dogs", JSON.stringify(favoriteDogs));
+	};
+
+	const isFavoriteDog = (dogName) => {
+		// Load favorite dogs array from local storage
+		var favoriteDogs = JSON.parse(window.localStorage.getItem("favorite_dogs"));
+
+		// If the array doesn't exist, then the dog is not favorite
+		if (!favoriteDogs) {
+			return false;
+		}
+
+		return favoriteDogs.includes(dogName);
+	};
+
+	const nextHandler = async (breed) => {
+		if (breed && breed.breedName) {
+			loadFilteredImage(breed.breedName);
+		}
+		else {
+			loadRandomImage();
+		}
+	}
+
+	const likeHandler = async (breed) => {
+		saveFavoriteDog(imageData.imageName);
+
+		if (breed && breed.breedName) {
+			loadFilteredImage(breed.breedName);
+		}
+		else {
+			loadRandomImage();
+		}
+	}
 
     return (
         <div>{imageData ?
@@ -41,8 +98,16 @@ function Photo(props) {
         <React.Fragment>
 			<h3> Breed: {capitalize(imageData.breed)}</h3>
             <Image src={imageData.message} alt="Image" width="250" />
-			<Button label="Next" onClick={() => props.breed && props.breed.breedName ? loadFilteredImage(props.breed.breedName) : loadRandomImage()}/>
-			<Button label="Like" onClick={() => props.breed && props.breed.breedName ? loadFilteredImage(props.breed.breedName) : loadRandomImage()}/>
+			<Button label="Next" onClick={ () => nextHandler(props.breed) }/>
+			{ isFavoriteDog(imageData.imageName) ?
+			(
+				<Button label="❤ Favorite" disabled={true} />
+			)
+			:
+			(
+				<Button label="Like" onClick={ () => likeHandler(props.breed)}/>
+			)
+			}
         </React.Fragment>
         )
 		:
